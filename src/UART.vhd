f9b65f2c78a8f74b570c092fe81f044ed084129e
ndiocson <ndiocson@outlook.com>
2019-10-11 23:14:14 -0600

    added src folder

---
+++ src/UART.vhd
@@ -0,0 +1,33 @@
+----------------------------------------------------------------------------------
+-- Company: N/A
+-- Engineer: Nick Diocson
+-- 
+-- Create Date: 10/11/2019 11:12:20 PM
+-- Design Name: UART
+-- Module Name: UART - Behavioral
+-- Project Name: UART
+-- Target Devices: Arty A7-35T
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity UART is
+--  Port ( );
+end UART;
+
+architecture Behavioral of UART is
+
+begin
+
+
+end Behavioral;
