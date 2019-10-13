84c0c5c149dfb2662dfcc34d451abfc7f791c9e2
ndiocson <ndiocson@outlook.com>
2019-10-13 10:16:02 -0600

    added simple counter and corresponding testbench

---
+++ src/Counter.vhd
@@ -0,0 +1,59 @@
+----------------------------------------------------------------------------------
+-- Company: N/A
+-- Engineer: Nick Diocson
+-- 
+-- Create Date: 10/13/2019 09:58:44 AM
+-- Design Name: Counter
+-- Module Name: Counter - Behavioral
+-- Project Name: UART
+-- Target Devices: Arty A7-35T
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Counter is
+    Generic (
+            CLK_FREQ        : positive := 1E7;      -- on-board clock frequency (10 MHz)
+            MAX_COUNT       : positive := 520       -- maximum number of cycles to count to
+            );
+    Port ( 
+            clk, reset      : in std_logic;
+            max_reached     : out std_logic
+            );
+end entity Counter;
+
+architecture Behavioral of Counter is
+
+-- count:   Shared variable to keep track of current count
+shared variable count  : integer := 0;
+
+begin
+    
+    count_proc: process(clk, reset) is
+    begin
+        if (rising_edge(clk)) then
+            if (reset = '1') then
+                count := 0;
+                max_reached <= '0';
+            elsif (count >= MAX_COUNT) then
+                count := 0;
+                max_reached <= '1';
+            else
+                count := count + 1;
+                max_reached <= '0';
+            end if;
+        end if;
+    end process count_proc;
+
+end architecture Behavioral;
---
+++ testbenches/Counter_Tb.vhd
@@ -0,0 +1,77 @@
+----------------------------------------------------------------------------------
+-- Company: N/A
+-- Engineer: Nick Diocson
+-- 
+-- Create Date: 10/13/2019 10:07:04 AM
+-- Design Name: Counter Testbench
+-- Module Name: Counter_Tb - Test
+-- Project Name: UART
+-- Target Devices: Arty A7-35T
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity Counter_Tb is
+end Counter_Tb;
+
+architecture Test of Counter_Tb is
+
+component Counter is
+    Generic (
+            CLK_FREQ        : positive := 1E7;      -- on-board clock frequency (10 MHz)
+            MAX_COUNT       : positive := 520       -- maximum number of cycles to count to
+            );
+    Port ( 
+            clk, reset      : in std_logic;
+            max_reached     : out std_logic
+            );
+end component Counter;
+
+-- Simulatted Clock Period
+constant CLK_PERIOD     : time := 100 ns;
+
+-- Input Signals
+signal clk              : std_logic := '0';
+signal reset            : std_logic := '0';
+
+-- Output Signal
+signal max_reached      : std_logic := '0';
+
+begin
+    
+    -- Instatiates device under test
+    DUT: entity work.Counter(Behavioral)
+        Generic Map (CLK_FREQ => open, MAX_COUNT => open)
+        Port Map (clk => clk, reset => reset, max_reached => max_reached);
+
+    -- Drives input clk signal
+    drive_clk: process is
+    begin
+        clk <= '1';
+        wait for CLK_PERIOD / 2;
+        clk <= '0';
+        wait for CLK_PERIOD / 2;
+    end process drive_clk;
+    
+    -- Process to sitmulate input signals of DUT
+    stimulus: process is
+    begin
+        wait for 400 ms;
+        reset <= '1';
+        wait for 20 ms;
+        reset <= '0';
+        wait;
+    end process stimulus;
+
+end architecture Test;
