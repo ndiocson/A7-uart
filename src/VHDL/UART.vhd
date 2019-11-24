d38993ce4b1755cb6f279f03f579d4301866d948
Nick Diocson <ndiocson@outlook.com>
2019-11-24 09:24:02 -0700

    added code for combined UART module

---
+++ src/VHDL/UART.vhd
@@ -0,0 +1,84 @@
+----------------------------------------------------------------------------------
+-- Company: N/A
+-- Engineer: Nick Diocson
+-- 
+-- Create Date: 11/24/2019 07:50:13 AM
+-- Design Name: UART
+-- Module Name: UART - Behavioral
+-- Project Name: UART
+-- Target Devices: Arty A7-35T
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity UART is
+    Generic (
+            BAUD_RATE       : positive := 9600;
+            BIT_CNT         : positive := 651;
+            SAMPLE_CNT      : positive := 325;
+            TRAN_BITS       : positive := 8
+            );
+    Port (
+            clk, reset      : in std_logic;
+            transmit        : in std_logic;
+            tx_data         : in std_logic_vector(TRAN_BITS - 1 downto 0);
+            rx_data         : out std_logic_vector(TRAN_BITS - 1 downto 0);
+            Rx              : in std_logic;
+            Tx              : out std_logic 
+            );
+end entity UART;
+
+architecture Behavioral of UART is
+
+component UART_Rx is
+    Generic (
+            BAUD_RATE       : positive := 9600;
+            BIT_CNT         : positive := 651;
+            SAMPLE_CNT      : positive := 325;
+            TRAN_BITS       : positive := 8
+            );
+    Port (
+            clk, reset      : in std_logic;
+            input_stream    : in std_logic;
+            rx_bits         : out std_logic_vector(TRAN_BITS - 1 downto 0)
+            );
+end component UART_Rx;
+
+component UART_Tx is
+    Generic (
+            BAUD_RATE       : positive := 9600;
+            BIT_CNT         : positive := 651;
+            SAMPLE_CNT      : positive := 325;
+            TRAN_BITS       : positive := 8
+            );
+    Port (
+            clk, reset      : in std_logic;
+            transmit        : in std_logic;
+            tx_bits         : in std_logic_vector(TRAN_BITS - 1 downto 0);
+            output_stream   : out std_logic
+            );
+end component UART_Tx;
+
+begin
+
+    -- Instantiates a UART receiver
+    Rx_module: UART_Rx
+        Generic Map(BAUD_RATE => BAUD_RATE, BIT_CNT => BIT_CNT, SAMPLE_CNT => SAMPLE_CNT, TRAN_BITS => TRAN_BITS)
+        Port Map (clk => clk, reset => reset, input_stream => Rx, rx_bits => rx_data);
+
+    -- Instantiates a UART transmitter
+    Tx_module: UART_Tx
+        Generic Map(BAUD_RATE => BAUD_RATE, BIT_CNT => BIT_CNT, SAMPLE_CNT => SAMPLE_CNT, TRAN_BITS => TRAN_BITS)
+        Port Map(clk => clk, reset => reset, transmit => transmit, tx_bits => tx_data, output_stream => Tx);
+
+end architecture Behavioral;
--- testbenches/VHDL/UART_Tb.vhd
+++ testbenches/VHDL/UART_Tb.vhd
@@ -26,34 +26,22 @@ end entity UART_Tb;
 
 architecture Test of UART_Tb is
 
-component UART_Rx is
+component UART is
     Generic (
             BAUD_RATE       : positive := 9600;
-            BIT_CNT         : positive := 10416;
-            SAMPLE_CNT      : positive := 5208;
-            TRAN_BITS       : positive := 8
-            );
-    Port (
-            clk, reset      : in std_logic;
-            input_stream    : in std_logic;
-            rx_bits         : out std_logic_vector(TRAN_BITS - 1 downto 0)
-            );
-end component UART_Rx;
-
-component UART_Tx is
-    Generic (
-            BAUD_RATE       : positive := 9600;
-            BIT_CNT         : positive := 10416;
-            SAMPLE_CNT      : positive := 5208;
+            BIT_CNT         : positive := 651;
+            SAMPLE_CNT      : positive := 325;
             TRAN_BITS       : positive := 8
             );
     Port (
             clk, reset      : in std_logic;
             transmit        : in std_logic;
-            tx_bits         : in std_logic_vector(TRAN_BITS - 1 downto 0);
-            output_stream   : out std_logic
+            tx_data         : in std_logic_vector(TRAN_BITS - 1 downto 0);
+            rx_data         : out std_logic_vector(TRAN_BITS - 1 downto 0);
+            Rx              : in std_logic;
+            Tx              : out std_logic 
             );
-end component UART_Tx;
+end component UART;
 
 -- CLK_PERIOD:          Simulated Clock Period
 -- BAUD_RATE:           9600 bits per second
@@ -62,33 +50,40 @@ end component UART_Tx;
 -- TRAN_BITS:           Number of transmission bits
 constant CLK_PERIOD     : time := 10 ns;
 constant BAUD_RATE      : positive := 9600;
-constant BIT_CNT        : positive := 10416;
-constant SAMPLE_CNT     : positive := 5208;
+constant BIT_CNT        : positive := 651;
+constant SAMPLE_CNT     : positive := 325;
 constant TRAN_BITS      : positive := 32;
 
 -- data_stream:         Signal to be transmitted to and received from by both DUTs
-signal data_stream      : std_logic;
+--signal data_stream      : std_logic;
+signal stream_1         : std_logic;
+signal stream_2         : std_logic;
 
 -- Input Signals
 signal clk              : std_logic := '0';
-signal reset            : std_logic := '0';
-signal transmit         : std_logic := '0';
-signal tx_bits          : std_logic_vector(TRAN_BITS - 1 downto 0);
+signal reset_1          : std_logic := '0';
+signal reset_2          : std_logic := '0';
+signal transmit_1       : std_logic := '0';
+signal transmit_2       : std_logic := '0';
+signal tx_data_1        : std_logic_vector(TRAN_BITS - 1 downto 0);
+signal tx_data_2        : std_logic_vector(TRAN_BITS - 1 downto 0);
 
 -- Output Signal
-signal rx_bits          : std_logic_vector(TRAN_BITS - 1 downto 0);
+signal rx_data_1        : std_logic_vector(TRAN_BITS - 1 downto 0);
+signal rx_data_2        : std_logic_vector(TRAN_BITS - 1 downto 0);
+
 
 begin
     
-    -- Instantiates receiver device under test
-    DUT_Rx: UART_Rx
+    -- Instantiates first UART device under test
+    DUT_1: UART
         Generic Map(BAUD_RATE => BAUD_RATE, BIT_CNT => BIT_CNT, SAMPLE_CNT => SAMPLE_CNT, TRAN_BITS => TRAN_BITS)
-        Port Map (clk => clk, reset => reset, input_stream => data_stream, rx_bits => rx_bits);
-
-    -- Instantiates tranceiver device under test
-    DUT_Tx: UART_Tx
+        Port Map (clk => clk, reset => reset_1, transmit => transmit_1, tx_data => tx_data_1, rx_data => rx_data_1, Rx => stream_1, Tx => stream_2);
+        
+    -- Instantiates second UART device under test
+    DUT_2: UART
         Generic Map(BAUD_RATE => BAUD_RATE, BIT_CNT => BIT_CNT, SAMPLE_CNT => SAMPLE_CNT, TRAN_BITS => TRAN_BITS)
-        Port Map(clk => clk, reset => reset, transmit => transmit, tx_bits => tx_bits, output_stream => data_stream);
+        Port Map (clk => clk, reset => reset_2, transmit => transmit_2, tx_data => tx_data_2, rx_data => rx_data_2, Rx => stream_2, Tx => stream_1);
 
     -- Drives input clk signal
     drive_clk: process is
@@ -100,39 +95,67 @@ begin
     end process drive_clk;
     
     -- Process to stimulate reset signal
-    reset_stim: process is
-    begin
-        wait for 9 ms;
-        reset <= '1';
-        wait for 5 us;
-        reset <= '0';
-        wait;
-    end process reset_stim;
+--    reset_stim: process is
+--    begin
+--        wait for 8 ms;
+--        reset_1 <= '1';
+--        reset_2 <= '1';
+--        wait for 5 us;
+--        reset_1 <= '0';
+--        reset_2 <= '0';
+--        wait;
+--    end process reset_stim;
     
-    -- Process to stimulate input signals of both DUTss
-    stimulus: process is
+    -- Process to stimulate first data stream
+    stimulus_1: process is
     begin
         wait for 100 us;
-        tx_bits <= "01010101010101010101010101010101";
-        wait for 3000 us;
-        transmit <= '1';
+        tx_data_1 <= "01010101010101010101010101010101";
+        wait for 200 us;
+        transmit_1 <= '1';
         wait for 5 us;
-        transmit <= '0';
+        transmit_1 <= '0';
         
-        wait for 1000 us;
-        tx_bits <= "00000000000000000000000000000000";
-        wait for 3000 us;
-        transmit <= '1';
+        wait for 500 us;
+        tx_data_1 <= "00000000000000000000000000000000";
+        wait for 200 us;
+        transmit_1 <= '1';
         wait for 5 us;
-        transmit <= '0';
+        transmit_1 <= '0';
         
-        wait for 1000 us;
-        tx_bits <= "11111111111111111111111111111111";
-        wait for 3000 us;
-        transmit <= '1';
+        wait for 300 us;
+        tx_data_1 <= "10101010101010101010101010101010";
+        wait for 100 us;
+        transmit_1 <= '1';
         wait for 5 us;
-        transmit <= '0';
+        transmit_1 <= '0';
         wait;
-    end process stimulus;
+    end process stimulus_1;
+
+    -- Process to stimulate second data stream
+    stimulus_2: process is
+    begin
+        wait for 300 us;
+        tx_data_2 <= "10101010101010101010101010101010";
+        wait for 100 us;
+        transmit_2 <= '1';
+        wait for 5 us;
+        transmit_2 <= '0';
+        
+        wait for 300 us;
+        tx_data_2 <= "11111111111111111111111111111111";
+        wait for 100 us;
+        transmit_2 <= '1';
+        wait for 5 us;
+        transmit_2 <= '0';
+        
+        wait for 500 us;
+        tx_data_2 <= "01010101010101010101010101010101";
+        wait for 200 us;
+        transmit_2 <= '1';
+        wait for 5 us;
+        transmit_2 <= '0';
+        wait;
+    end process stimulus_2;     
 
 end architecture Test;
