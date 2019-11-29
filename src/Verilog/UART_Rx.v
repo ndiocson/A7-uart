445c790b2699cb07b4a7e236db7f95a5d63b6be3
ndiocson <ndiocson@outlook.com>
2019-11-28 21:50:04 -0700

    added initial template for verilog files

---
+++ src/Verilog/UART_Rx.v
@@ -0,0 +1,8 @@
+module UART_Rx 
+(   
+    input logic clk, reset,
+    input logic input_stream,
+    output logic [31:0] rx_bits
+);
+
+endmodule: UART_Rx
---
+++ src/Verilog/UART_Tx.v
@@ -0,0 +1,9 @@
+module UART_Tx
+(
+    input logic clk, reset,
+    input logic transmit,
+    input logic [31:0] tx_bits,
+    output logic output_stream
+);
+
+endmodule: UART_Tx
